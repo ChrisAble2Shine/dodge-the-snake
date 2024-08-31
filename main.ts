8647ceaea4b21be89ec1678fb444d7b38b12e3bb
ChrisAble2Shine <148521238+ChrisAble2Shine@users.noreply.github.com>
2024-08-31 14:41:07 +0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.startEffect(effects.warmRadial)
     }
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
-	
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.gameOver(false)
 })
 let projectile: Sprite = null
 let mySprite: Sprite = null
@@ -27,6 +27,46 @@ mySprite = sprites.create(img`
     . . . . f f f f f f . . . . 
     . . . . f f . . f f . . . . 
     `, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+[img`
+    . . . . f f f f f . f f f . 
+    . . . f f c c c c f f f f f 
+    . . f c c c c c c b f f f f 
+    . . f c c c c c c 3 c f f f 
+    . f c c c c c c c c 3 3 f . 
+    . f c c 4 c c c c c f f f . 
+    . f f e 4 4 c c c f f f f . 
+    . f f e 4 4 f b f 4 4 f f . 
+    . . f f d d f 1 4 d 4 f . . 
+    . . . f d d d d 4 f f f . . 
+    . . . f e 4 4 4 e e f . . . 
+    . . . f 3 3 3 e d d 4 . . . 
+    . . . f 3 3 3 e d d e . . . 
+    . . . f 6 6 6 f e e f . . . 
+    . . . . f f f f f f . . . . 
+    . . . . . . f f f . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . 
+    . . . . f f f f f . f f f . 
+    . . . f f c c c c f f f f f 
+    . . f c c c c c c b f f f f 
+    . . f c c c c c c 3 c f f f 
+    . f c c c c c c c c 3 3 f . 
+    . f c c 4 c c c c c f f f . 
+    . f f c 4 4 c c c f f f f . 
+    . f f f 4 4 f b f 4 4 f f . 
+    . . f f d d f 1 4 d 4 f . . 
+    . . . f d d d e e f f f . . 
+    . . . f e 4 e d d 4 f . . . 
+    . . . f 3 3 e d d e f . . . 
+    . . f f 6 6 f e e f f f . . 
+    . . f f f f f f f f f f . . 
+    . . . f f f . . . f f . . . 
+    `],
+100,
+true
+)
 mySprite.ay = 250
 tiles.setCurrentTilemap(tilemap`level2`)
 scene.setBackgroundColor(6)
@@ -53,5 +93,96 @@ game.onUpdateInterval(1900, function () {
         . f 6 1 1 1 1 1 1 6 6 6 f . . . 
         . . c c c c c c c c c f . . . . 
         `, mySprite, -100, 0)
+    animation.runImageAnimation(
+    projectile,
+    [img`
+        . . . . c c c c c c . . . . . . 
+        . . . c 6 7 7 7 7 6 c . . . . . 
+        . . c 7 7 7 7 7 7 7 7 c . . . . 
+        . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+        . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+        . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+        . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+        . . f 7 7 7 7 6 c 7 7 6 f c . . 
+        . . . f c c c c 7 7 6 f 7 7 c . 
+        . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+        . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+        c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+        f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+        . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+        . . c c c c c c c c c f . . . . 
+        `,img`
+        . . . c c c c c c . . . . . . . 
+        . . c 6 7 7 7 7 6 c . . . . . . 
+        . c 7 7 7 7 7 7 7 7 c . . . . . 
+        c 6 7 7 7 7 7 7 7 7 6 c . . . . 
+        c 7 c 6 6 6 6 c 7 7 7 c . . . . 
+        f 7 6 f 6 6 f 6 7 7 7 f . . . . 
+        f 7 7 7 7 7 7 7 7 7 7 f . . . . 
+        . f 7 7 7 7 6 c 7 7 6 f . . . . 
+        . . f c c c c 7 7 6 f c c c . . 
+        . . c 6 2 7 7 7 f c c 7 7 7 c . 
+        . c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
+        . c 1 1 1 1 7 6 6 c 6 6 6 c c c 
+        . c 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . c 6 1 1 1 1 1 6 6 6 6 6 c . . 
+        . . c 6 1 1 1 1 1 7 6 6 c c . . 
+        . . . c c c c c c c c c c . . . 
+        `,img`
+        . . . . . c c c c c c c . . . . 
+        . . . . c 6 7 7 7 7 7 6 c . . . 
+        . . . c 7 c 6 6 6 6 c 7 6 c . . 
+        . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+        . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+        . . f 7 8 1 f f 1 6 7 7 7 f . . 
+        . . f 6 f 1 f f 1 f 7 7 7 f . . 
+        . . . f f 2 2 2 2 f 7 7 6 f . . 
+        . . c c f 2 2 2 2 7 7 6 f c . . 
+        . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+        c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+        f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+        f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+        . . f f c c c c c c c c . . . . 
+        `,img`
+        . . . . . . c c c c c c c . . . 
+        . . . . . c f f 6 6 f f 7 c . . 
+        . . . . c 7 6 6 6 6 6 6 7 6 c . 
+        . . . c 7 7 7 7 7 7 7 7 7 7 c . 
+        . . . c 7 8 1 f f 1 6 7 7 7 c . 
+        . . . f 6 f 1 f f 1 f 7 7 7 f . 
+        . . . f 6 f 2 2 2 2 f 7 7 7 f . 
+        . . c c 6 f 2 2 2 2 f 7 7 6 f . 
+        . c 7 7 7 7 2 2 2 2 7 7 f c . . 
+        c 7 1 1 1 7 7 7 7 7 c c 7 7 c . 
+        f 1 1 1 1 1 7 7 7 f c 6 7 7 7 c 
+        f 1 1 1 1 1 1 6 f c c 6 6 6 c c 
+        f 6 1 1 1 1 1 6 6 c 6 6 6 c . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . f 6 1 1 1 1 6 6 6 6 6 c . . . 
+        . . f f c c c c c c c c . . . . 
+        `,img`
+        . . . . . . c c c c c c c . . . 
+        . . . . . c f f 6 6 f f 7 c . . 
+        . . . . c 7 6 6 6 6 6 6 7 6 c . 
+        . . . c 7 7 7 7 7 7 7 7 7 7 c . 
+        . . . c 7 8 1 f f 1 6 7 7 7 c . 
+        . . . f 6 f 1 f f 1 f 7 7 7 f . 
+        . . . f 6 f 2 2 2 2 f 7 7 7 f . 
+        . . c c 6 f 2 2 2 2 f 7 7 6 f . 
+        . c 7 7 7 7 2 2 2 2 7 7 f c . . 
+        c 7 1 1 1 7 7 7 7 7 c c 7 7 c . 
+        f 1 1 1 1 1 7 7 7 f c 6 7 7 7 c 
+        f 1 1 1 1 1 1 6 f c c 6 6 6 c c 
+        f 6 1 1 1 1 1 6 6 c 6 6 6 c . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . f 6 1 1 1 1 6 6 6 6 6 c . . . 
+        . . f f c c c c c c c c . . . . 
+        `],
+    200,
+    false
+    )
     projectile.setPosition(136, 84)
 })
